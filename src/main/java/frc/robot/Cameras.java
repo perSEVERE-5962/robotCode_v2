373616a9b517fd55c074ef6db4a12446354b2d11
Team5962Programming <programming@mvrobotics.net>
2025-10-11 11:57:10 -0400

    Just small updates(new members merge to this)

--- src/main/java/frc/robot/Cameras.java
+++ src/main/java/frc/robot/Cameras.java
@@ -56,29 +56,25 @@ public enum Cameras {
    * Left Camera
    */
   LEFT_CAM("left",
-      new Rotation3d(0, Math.toRadians(-24.094), Math.toRadians(30)),
-      new Translation3d(Units.inchesToMeters(12.056),
-          Units.inchesToMeters(10.981),
-          Units.inchesToMeters(8.44)),
-      VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1)),
+      new Rotation3d(0, 0, 0),
+      new Translation3d(0.31,0.17,0.32),
+      VecBuilder.fill(0.3, 0.3, 0.6), VecBuilder.fill(0.1, 0.1, 0.2)),
   /**
    * Right Camera
    */
   RIGHT_CAM("right",
-      new Rotation3d(0, Math.toRadians(-24.094), Math.toRadians(-30)),
-      new Translation3d(Units.inchesToMeters(12.056),
-          Units.inchesToMeters(-10.981),
-          Units.inchesToMeters(8.44)),
-      VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1)),
+      new Rotation3d(0, 0, 0),
+      new Translation3d(0,0,0),
+          VecBuilder.fill(0.3, 0.3, 0.6), VecBuilder.fill(0.1, 0.1, 0.2));
   /**
    * Center Camera
    */
-  CENTER_CAM("center",
-      new Rotation3d(0, Units.degreesToRadians(18), 0),
-      new Translation3d(Units.inchesToMeters(-4.628),
-          Units.inchesToMeters(-10.687),
-          Units.inchesToMeters(16.129)),
-      VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1));
+  // CENTER_CAM("center",
+  //     new Rotation3d(0, Units.degreesToRadians(18), 0),
+  //     new Translation3d(Units.inchesToMeters(-4.628),
+  //         Units.inchesToMeters(-10.687),
+  //         Units.inchesToMeters(16.129)),
+  //     VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1));
 
   /**
    * Latency alert to use when high latency is detected.
