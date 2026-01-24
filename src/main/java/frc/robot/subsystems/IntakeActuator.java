26fb82d38e3fe3179e9de004e8bd22660fcc4ced
cardinal777 <138259383+cardinal778@users.noreply.github.com>
2026-01-24 14:24:35 -0500

    add constants for IntakeActuator

--- src/main/java/frc/robot/subsystems/IntakeActuator.java
+++ src/main/java/frc/robot/subsystems/IntakeActuator.java
@@ -6,7 +6,7 @@ public class IntakeActuator extends Actuator {
   private static IntakeActuator instance;
 
   public IntakeActuator() {
-    super(Constants.CANDeviceIDs.kIntakeActuatorID, 1.0, 0, 0, 0, 1.0, 0, 0, 0, 0, false, false, false);
+    super(Constants.CANDeviceIDs.kIntakeActuatorID, Constants.IntakeConstants.P, Constants.IntakeConstants.I, Constants.IntakeConstants.D, Constants.IntakeConstants.MinOutput, Constants.IntakeConstants.MaxOutput, Constants.IntakeConstants.FF, Constants.IntakeConstants.Iz, 0, 0, false, false, false);
   }
 
   public static IntakeActuator getInstance() {
