271b76911b8fbb509251e93cdfcc8b498906b17c
Tamara Nguyen <nguyentamara1010@gmail.com>
2026-02-25 10:43:57 -0500

    subsystem accessors: hardware getters, VisionFilter, JamProtection, Cameras std devs

--- src/main/java/frc/robot/subsystems/Agitator.java
+++ src/main/java/frc/robot/subsystems/Agitator.java
@@ -1,17 +1,31 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.spark.SparkLowLevel;
-import com.revrobotics.spark.SparkMax;
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkLowLevel;
+import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Constants.JamProtectionConstants;
+import frc.robot.util.JamProtection;
 
 public class Agitator extends SubsystemBase {
   private SparkMax motor;
   private SparkMaxConfig motorConfig;
   private static Agitator instance;
+
+  private final JamProtection jamProtection =
+      new JamProtection(
+          "Agitator",
+          JamProtectionConstants.AGITATOR_JAM_CURRENT_AMPS,
+          JamProtectionConstants.AGITATOR_JAM_VELOCITY_RPM,
+          JamProtectionConstants.AGITATOR_STARTUP_IGNORE_SEC,
+          JamProtectionConstants.AGITATOR_JAM_CONFIRM_SEC,
+          JamProtectionConstants.AGITATOR_REVERSE_SEC,
+          JamProtectionConstants.AGITATOR_COOLDOWN_SEC,
+          JamProtectionConstants.AGITATOR_REVERSE_POWER,
+          JamProtectionConstants.AGITATOR_MAX_ATTEMPTS);
 
   private Agitator() {
     motor = new SparkMax(Constants.CANDeviceIDs.kAgitatorID, SparkLowLevel.MotorType.kBrushless);
@@ -27,6 +41,51 @@ public class Agitator extends SubsystemBase {
 
   public void move(double speed) {
     motor.set(speed);
+  }
+
+  public double getTemperature() {
+    return motor.getMotorTemperature();
+  }
+
+  public double getAppliedOutput() {
+    return motor.getAppliedOutput();
+  }
+
+  public double getOutputCurrent() {
+    return motor.getOutputCurrent();
+  }
+
+  public double getVelocityRPM() {
+    return motor.getEncoder().getVelocity();
+  }
+
+  @Override
+  public void periodic() {
+    jamProtection.update(getOutputCurrent(), getVelocityRPM(), isRunning());
+    double override = jamProtection.getMotorOverride();
+    if (!Double.isNaN(override)) {
+      motor.set(override);
+    }
+  }
+
+  public boolean isRunning() {
+    return Math.abs(motor.getAppliedOutput()) > 0.05;
+  }
+
+  public JamProtection getJamProtection() {
+    return jamProtection;
+  }
+
+  public SparkMax getMotor() {
+    return motor;
+  }
+
+  public int getStickyFaultsRaw() {
+    try {
+      return (int) motor.getStickyFaults().rawBits;
+    } catch (Throwable t) {
+      return -1;
+    }
   }
 
   public static Agitator getInstance() {
